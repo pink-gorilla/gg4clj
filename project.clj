033d4b3474bed7fed90cf52de0408c736ba03240
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-10-17 15:48:40 +0200

    bugfix namespace

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 ;;;;
 ;;;; gg4clj is licenced to you under the MIT licence. See the file LICENCE.txt for full details.
 
-(defproject pinkgorilla.ui.gg4clj "0.1.7"
+(defproject pinkgorilla.ui.gg4clj "0.1.9"
   :description "A simple wrapper for R's ggplot2 in Clojure and Gorilla REPL."
   :url "https://github.com/pinkgorillawb/gg4clj"
   :license {:name "MIT"}
@@ -13,7 +13,8 @@
                              :sign-releases false}]]
   
   :dependencies 
-  [[org.clojure/clojure "1.10.0"]
+  [;[org.clojure/clojure "1.10.0"]
+   [org.clojure/clojure "1.9.0-alpha14"] ; downgrade to pinggorilla version
    [org.clojars.deas/gorilla-renderable "2.1.0"] ;PinkGorilla Renderable
    ]
   :plugins [
